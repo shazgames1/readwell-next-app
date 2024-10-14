12004a898a8489942aeef364299e2c9aac2edf7b
X <durov@t.me>
2024-10-15 04:14:58 +0700

    Playing with ci/cd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,38 @@
-import Image from "next/image";
+import { db } from "@/db/drizzle"
+import { usersTable } from "@/db/schema"
+import { revalidatePath } from "next/cache"
+import Image from "next/image"
+import { randEmail, randUserName, randNumber } from "@ngneat/falso"
 
-export default function Home() {
+export default async function Home() {
+  const users = await db.select().from(usersTable)
+
+  async function generateUser() {
+    "use server"
+
+    await db.insert(usersTable).values({
+      age: randNumber({ min: 16, max: 40 }),
+      email: randEmail(),
+      name: randUserName(),
+    } as typeof usersTable.$inferInsert)
+
+    revalidatePath("/")
+  }
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <h1>Users:</h1>
+        <ul>
+          {users.map((u) => (
+            <li>
+              {u.name} ({u.id}) {u.email}
+            </li>
+          ))}
+        </ul>
+        <form action={generateUser}>
+          <button>Add new random user</button>
+        </form>
         <Image
           className="dark:invert"
           src="https://nextjs.org/icons/next.svg"
@@ -12,7 +41,7 @@ export default function Home() {
           height={38}
           priority
         />
-        <h1>Aloha!:))</h1>
+        <h1>Aloha!!!!:)) Ura ya nastroil docker for dev!!!)</h1>
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Get started by editing{" "}
@@ -23,32 +52,6 @@ export default function Home() {
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
@@ -98,5 +101,5 @@ export default function Home() {
         </a>
       </footer>
     </div>
-  );
+  )
 }
