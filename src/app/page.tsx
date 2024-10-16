69dfb9efb94fab39374946c373548f3bb706b2d9
X <durov@t.me>
2024-10-17 01:00:41 +0700

    UI update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,7 +37,7 @@ export default async function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
-          Here are all users of app:
+          Users ({ users.length }):
         </h3>
         <ul>
           {users.map((u) => (
