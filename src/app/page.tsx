600e07be8f36443b8980689447132bdd72189fef
X <durov@t.me>
2024-10-16 20:08:42 +0700

    Testing github cache

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,7 +36,9 @@ export default async function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1>Users:</h1>
+        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+          Users:
+        </h1>
         <ul>
           {users.map((u) => (
             <li key={u.id}>
