ec255b042fc38c582710327fc8f1d72f8270f795
X <durov@t.me>
2024-10-16 17:53:33 +0700

    Added contry for users table. Testing migrations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { db } from "@/db/drizzle"
 import { usersTable } from "@/db/schema"
 import { revalidatePath } from "next/cache"
 import Image from "next/image"
-import { randEmail, randUserName, randNumber } from "@ngneat/falso"
+import { randEmail, randUserName, randNumber, randCountry } from "@ngneat/falso"
 
 export default async function Home() {
   const users = await db.select().from(usersTable)
@@ -14,6 +14,7 @@ export default async function Home() {
       age: randNumber({ min: 16, max: 40 }),
       email: randEmail(),
       name: randUserName(),
+      country: randCountry()
     } as typeof usersTable.$inferInsert)
 
     revalidatePath("/")
@@ -26,80 +27,14 @@ export default async function Home() {
         <ul>
           {users.map((u) => (
             <li key={u.id}>
-              {u.name} ({u.id}) {u.email}
+              {u.name} ({u.id}) {u.email} from {u.country}
             </li>
           ))}
         </ul>
         <form action={generateUser}>
           <button>Add new random user</button>
         </form>
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <h1>Aloha!!!!:)) Ura ya nastroil docker for dev!!!)</h1>
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   )
 }
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -6,4 +6,5 @@ export const usersTable = pgTable("users", {
   age: integer().notNull(),
   email: varchar({ length: 255 }).notNull().unique(),
   notes: text(),
+  country: varchar(),
 })
