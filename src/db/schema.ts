7f2c7a7092cfb3ce33c8057817d1d08ecda1d12d
X <durov@t.me>
2024-10-16 17:44:15 +0700

    Changed the approach to applying database migrations: the logic for starting migrations has been moved from ci/cd to docker compose

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,8 +1,9 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core"
+import { integer, pgTable, text, varchar } from "drizzle-orm/pg-core"
 
 export const usersTable = pgTable("users", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   name: varchar({ length: 255 }).notNull(),
   age: integer().notNull(),
   email: varchar({ length: 255 }).notNull().unique(),
+  notes: text(),
 })
