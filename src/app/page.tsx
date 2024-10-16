1e8f3fa127d3bf3a59ae8cac22defe4585b8f070
X <durov@t.me>
2024-10-16 20:31:25 +0700

    Cache test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,7 +37,7 @@ export default async function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-          All users:
+          All users of app:
         </h1>
         <ul>
           {users.map((u) => (
